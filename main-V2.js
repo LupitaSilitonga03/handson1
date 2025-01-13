95b0653ea730da0c844b01eebd12ef4c271d857b
lupitasilitonga <2381017@unai.edu>
2025-01-13 11:02:03 +0700

    first commit

---
+++ main-V2.js
@@ -0,0 +1,10 @@
+var objectHuman = {
+    name: "John",
+    age: 20,
+    location: "Jakarta",
+    motor: "Honda",
+};
+function printHuman(human) {
+    console.log(human.location);
+}
+printHuman(objectHuman);
---
+++ main.js
@@ -0,0 +1,18 @@
+/*
+let num1 = 20;
+let num2 = 90;
+console.log(num1 + num2);
+*/
+
+const human = {
+  name: "John Does",
+  age: 30,
+  gender: "Male",
+  isAlive: true,
+};
+
+function cetakHuman(human) {
+  console.log(human.age);
+}
+
+cetakHuman(human);
